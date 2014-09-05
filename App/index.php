0369af178468878114d4064155f74c1b92900ee1
Motedyn <motedyn@gmail.com>
2014-09-04 21:41:12 -0600

    actualizacion
    actualizacion **

---
+++ App/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+</head>
+<body>
+
+
+
+
+
+</body>
+</html>
